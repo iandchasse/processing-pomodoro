f55e7b189b704b0ba95231eaa15453233ceb62f0
iandchasse <86565956+iandchasse@users.noreply.github.com>
2021-06-27 15:46:46 -0400

    Add files via upload

--- source/pomodoro1point0.java
+++ source/pomodoro1point0.java
@@ -16,48 +16,35 @@ public class pomodoro1point0 extends PApplet {
 
 
 long startTime = System.nanoTime();
-long pauseTime;
+long deltaTime = 0;
+long pauseTime = 0;
 int sec; int min;
-boolean running = false;
 int state;
+int prevState;
 
 public void setup() {
-  System.out.println("Hello World");
+  surface.setTitle("Pomodoro by Ian Chasse, 2021");
+  surface.setResizable(true);
   
   textSize(110);
   textAlign(CENTER);
   state = 1; // in a 25 min lap
   
-  //clock();
   
 }
 
-//void clock() {
-//  int sec = (int)(((System.nanoTime() - startTime) / 1000000000));
-//  int min = sec / 60;
-//  if (min < 25){
-//    sec = (int)(((System.nanoTime() - startTime) / 1000000000));
-//    min = sec / 60;
-//    draw();
-//  }
-//  println("yo");
-//  startTime = System.nanoTime();
-//  sec = (int)(((System.nanoTime() - startTime) / 1000000000));
-//  min = sec / 60;
-//  println(min);
-//  if (min < 5) {
-//    sec = (int)(((System.nanoTime() - startTime) / 1000000000));
-//    min = sec / 60;
-//    draw();
-//  }
-//  startTime = System.nanoTime();
-//}
-
 public void draw() {
-  println(state);
+  println("Pause time: " + pauseTime);
+  println("Pause time: " + startTime);
   background(30);
-  sec = (int)(((System.nanoTime() - startTime) / 1000000000));
-  min = sec / 60;
+  if (state != 3) {
+    sec = (int)((((System.nanoTime() - deltaTime) - startTime) / 1000000000));
+    min = sec / 60;
+  }
+  else {
+    sec = (int)(((pauseTime - startTime) / 1000000000));
+    min = sec / 60;
+  }
   String display = (String.format("%02d", min) + ":" + String.format("%02d" , sec % 60));
   if (min >= 25 && state == 1) {
     state = 2;
@@ -65,26 +52,35 @@ public void draw() {
       
   }
   if (min >= 5 && state == 2) {
-    state = 2;
+    state = 1;
     startTime = System.nanoTime(); sec = 0; min = 0;
-  }
+  }  
   text(display, 300, 200);
+  
+  
   switch(state){
     case 1: text("Study time!", 300, 350); break;
     case 2: text("Break time!", 300, 350); break;
+    case 3: text("Paused.", 300, 350); break;
   }
 }
 
 
-//void pauseButton(running, elapsed) {
-//  if (running) {
-    
-//  }
-//  else {
-//    pauseTime = System.nanoTime();
-    
-//  }
-//}
+public void mouseClicked() {
+  if (state == 3) {
+    state = prevState;
+    prevState = 3;
+    deltaTime = System.nanoTime() - pauseTime;
+  }
+  else {
+     prevState = state;
+     state = 3;
+     pauseTime = System.nanoTime();
+     
+     println("paused"); 
+  }
+  
+}
   public void settings() {  size(600,400); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "pomodoro1point0" };
--- source/pomodoro1point0.pde
+++ source/pomodoro1point0.pde
@@ -1,47 +1,34 @@
 
 long startTime = System.nanoTime();
-long pauseTime;
+long deltaTime = 0;
+long pauseTime = 0;
 int sec; int min;
-boolean running = false;
 int state;
+int prevState;
 
 void setup() {
-  System.out.println("Hello World");
+  surface.setTitle("Pomodoro by Ian Chasse, 2021");
+  surface.setResizable(true);
   size(600,400);
   textSize(110);
   textAlign(CENTER);
   state = 1; // in a 25 min lap
   
-  //clock();
   
 }
 
-//void clock() {
-//  int sec = (int)(((System.nanoTime() - startTime) / 1000000000));
-//  int min = sec / 60;
-//  if (min < 25){
-//    sec = (int)(((System.nanoTime() - startTime) / 1000000000));
-//    min = sec / 60;
-//    draw();
-//  }
-//  println("yo");
-//  startTime = System.nanoTime();
-//  sec = (int)(((System.nanoTime() - startTime) / 1000000000));
-//  min = sec / 60;
-//  println(min);
-//  if (min < 5) {
-//    sec = (int)(((System.nanoTime() - startTime) / 1000000000));
-//    min = sec / 60;
-//    draw();
-//  }
-//  startTime = System.nanoTime();
-//}
-
 void draw() {
-  println(state);
+  println("Pause time: " + pauseTime);
+  println("Pause time: " + startTime);
   background(30);
-  sec = (int)(((System.nanoTime() - startTime) / 1000000000));
-  min = sec / 60;
+  if (state != 3) {
+    sec = (int)((((System.nanoTime() - deltaTime) - startTime) / 1000000000));
+    min = sec / 60;
+  }
+  else {
+    sec = (int)(((pauseTime - startTime) / 1000000000));
+    min = sec / 60;
+  }
   String display = (String.format("%02d", min) + ":" + String.format("%02d" , sec % 60));
   if (min >= 25 && state == 1) {
     state = 2;
@@ -49,23 +36,32 @@ void draw() {
       
   }
   if (min >= 5 && state == 2) {
-    state = 2;
+    state = 1;
     startTime = System.nanoTime(); sec = 0; min = 0;
-  }
+  }  
   text(display, 300, 200);
+  
+  
   switch(state){
     case 1: text("Study time!", 300, 350); break;
     case 2: text("Break time!", 300, 350); break;
+    case 3: text("Paused.", 300, 350); break;
   }
 }
 
 
-//void pauseButton(running, elapsed) {
-//  if (running) {
-    
-//  }
-//  else {
-//    pauseTime = System.nanoTime();
-    
-//  }
-//}
+void mouseClicked() {
+  if (state == 3) {
+    state = prevState;
+    prevState = 3;
+    deltaTime = System.nanoTime() - pauseTime;
+  }
+  else {
+     prevState = state;
+     state = 3;
+     pauseTime = System.nanoTime();
+     
+     println("paused"); 
+  }
+  
+}
